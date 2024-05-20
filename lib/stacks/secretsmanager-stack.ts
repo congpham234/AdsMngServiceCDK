cb3409b431254ace51c4dcbdd7f93feffa8eae1d
Cong pham <>
2024-05-19 22:31:02 -0700

    Clean up doomed API keys

--- lib/stacks/secretsmanager-stack.ts
+++ lib/stacks/secretsmanager-stack.ts
@@ -9,28 +9,10 @@ export interface SecretsManagerStackProps extends StackProps {
 }
 
 export class SecretsManagerStack extends Stack {
-  readonly bookingDotCom: Secret;
-  readonly openAiApiKey: Secret;
-  readonly googlePlacesApiKey: Secret;
   readonly secrets: Secret;
 
   constructor(scope: Construct, id: string, props: SecretsManagerStackProps) {
     super(scope, id, props);
-
-    this.bookingDotCom = new Secret(this, 'BookingDotComAPIKeyId', {
-      secretName: 'BookingDotComAPIKey',
-      description: 'This is a secret for booking dot com Rapid API.',
-    });
-
-    this.openAiApiKey = new Secret(this, 'OpenAiAPIKeyId', {
-      secretName: 'OpenAiAPIKeyId',
-      description: 'This is a secret for OpenAi API key.',
-    });
-
-    this.googlePlacesApiKey = new Secret(this, 'GooglePlacesAPIKeyId', {
-      secretName: 'GooglePlacesAPIKey',
-      description: 'This is a secret for Google Places API key.',
-    });
 
     this.secrets = new Secret(this, `${SERVICE_NAME}ExternalAPIKeyId`, {
       secretName: `${SERVICE_NAME}ExternalAPIKeyId`,
@@ -38,10 +20,6 @@ export class SecretsManagerStack extends Stack {
     });
 
     this.secrets.grantRead(props.serviceRole);
-
-    this.exportValue(this.bookingDotCom.secretArn);
-    this.exportValue(this.openAiApiKey.secretArn);
-    this.exportValue(this.googlePlacesApiKey.secretArn);
     this.exportValue(this.secrets.secretArn);
   }
 }
