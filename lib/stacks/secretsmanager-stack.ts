1917357dbbc814b396b230630a0f139c924fda8d
Cong pham <>
2024-05-19 22:06:53 -0700

    Try to remove the permission first before deleting

--- lib/stacks/secretsmanager-stack.ts
+++ lib/stacks/secretsmanager-stack.ts
@@ -9,16 +9,33 @@ export interface SecretsManagerStackProps extends StackProps {
 }
 
 export class SecretsManagerStack extends Stack {
+  readonly bookingDotCom: Secret;
+  readonly openAiApiKey: Secret;
+  readonly googlePlacesApiKey: Secret;
   readonly secrets: Secret;
 
   constructor(scope: Construct, id: string, props: SecretsManagerStackProps) {
     super(scope, id, props);
 
+    this.bookingDotCom = new Secret(this, 'BookingDotComAPIKeyId', {
+      secretName: 'BookingDotComAPIKey',
+      description: 'This is a secret for booking dot com Rapid API.',
+    });
+
+    this.openAiApiKey = new Secret(this, 'OpenAiAPIKeyId', {
+      secretName: 'OpenAiAPIKeyId',
+      description: 'This is a secret for OpenAi API key.',
+    });
+
+    this.googlePlacesApiKey = new Secret(this, 'GooglePlacesAPIKeyId', {
+      secretName: 'GooglePlacesAPIKey',
+      description: 'This is a secret for Google Places API key.',
+    });
+
     this.secrets = new Secret(this, `${SERVICE_NAME}ExternalAPIKeyId`, {
       secretName: `${SERVICE_NAME}ExternalAPIKeyId`,
       description: 'This is a secret for all external api keys.',
     });
-
     this.secrets.grantRead(props.serviceRole);
   }
 }
